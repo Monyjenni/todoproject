4136cc81555dbc35990adf235e4cb4b61384c937
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-30 15:07:38 +0700

    adding_app_bar

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+          :to="item.to"
           link
         >
           <v-list-item-icon>
@@ -35,11 +36,42 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar
+    app
+    color="#43a047"
+    dark
+  
+    prominent
+    src="https://picsum.photos/1920/1080?random"
+    fade-img-on-scroll
+    scroll-target="#scrolling-techniques-5"
+    scroll-threshold="500"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+      ></v-img>
+    </template>
 
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-app-bar-title>Title</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
 <!-- adding router  -->
     <v-main>
       <router-view></router-view>
@@ -52,8 +84,8 @@
     data: () => ({ 
       drawer: null,
       items: [
-          { title: 'Todo', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Todo', icon: 'mdi-view-list', to: '/' },
+          { title: 'About', icon: 'mdi-help-box', to: '/about' },
         ], }),
   }
 </script>
