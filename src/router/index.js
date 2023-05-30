74ad17bd1b07de9ed36de11dcb129f181b2c552d
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-30 14:24:00 +0700

    adding_router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,31 @@
-// Import Vue and Vue Router
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomeView from '../../views/Home.vue'
 
-// Import your components
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-
-
-// Use Vue Router
 Vue.use(VueRouter)
 
-// Define routes
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../../views/About.vue')
+    }
+  }
 ]
 
-// Create the router instance
 const router = new VueRouter({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
-// Export the router instance
 export default router
